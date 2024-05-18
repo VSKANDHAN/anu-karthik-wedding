63409f970a371de84677a3995fdc588714bb522b
vsk <kandhanvsk@gmail.com>
2024-05-18 16:12:52 +0530

    seconds update

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
     setTimeout(function() {
         preloader.style.display = 'none';
         content.style.display = 'block';
-    }, 1000); // 2000 milliseconds = 2 seconds
+    }, 600); // 2000 milliseconds = 2 seconds
 });
   
   
